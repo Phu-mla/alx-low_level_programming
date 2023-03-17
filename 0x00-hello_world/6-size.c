8f97bfcedb7caac3a798812482d902997ea30cdc
Phumla Moloi <phumlah.m@gmail.com>
2023-03-16 17:07:51 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+printf("size of a char : " sizeof(char) " byte\(s\)\n");
+r
+	return (0);
+}
