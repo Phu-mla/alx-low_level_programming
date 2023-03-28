51de3fd949204baa34f59147467ac4cf7302fa03
Phumla Moloi <phumlah.m@gmail.com>
2023-03-28 13:04:54 -0700

    initial commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- *
- * reset_to_98 - takes a pointer to an int and updates
- * the value it point to to 98
+ * reset_to_98 - takes a pointer to an int and updates the value it point to, to 98
  * @n: pointer to int
  */
-void reset_to_98(int*n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,11 @@
  * @a: first integer to swap
  * @b: second integer to swap
  */
-void swap_int(int*a, int*b)
+void swap_int(int *a, int *b)
 {
 	int swap;
 	swap = *a;
+
 	*a = *b;
 	*b = swap;
 }
