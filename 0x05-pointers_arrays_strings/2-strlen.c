d72ac7a9ea79d9190cc53b380466da64ff3a73a4
Phumla Moloi <phumlah.m@gmail.com>
2023-03-28 13:49:32 -0700

    betty style corrections

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return count;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -20,6 +20,20 @@ void swap_int(int*a, int*b);
  * swap_int - swaps the value of two ints
  * @a: first integer to swap
  * @b: second integer to swap
+ *
+ */
+
+int _strlen(char *s);
+/**
+ * _strlen - calculates the number of characters in a string
+ * @s: the string whose characters we are calculating
+ * Return: the number of characters in the string
+ */
+
+void _puts(char *str);
+/**
+ * _puts - prints a string followed by a newline to stdout
+ * @str: string to be printed out
  */
 
 #endif
