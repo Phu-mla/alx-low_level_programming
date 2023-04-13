729d352b05749af8c7355f06d91cacdaeac8b85c
Phumla Moloi <phumlah.m@gmail.com>
2023-04-13 16:46:13 -0700

    project 0x0C initial commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - create array of integers
+ * @min: start 
+ * @max: end 
+ * Return: pointer to array
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+	int n = (max - min + 1);
+
+	if (min > max) 
+		return (NULL);
+
+	p = malloc(sizeof(int) * n);
+
+	if (p == NULL)
+		return (NULL);
+
+	
+	for (i = 0; i < n; i++)
+		p[i] = min++;
+
+	return (p);
+}
